db88d5a572455c1916736760d07220c8e81f01cc
spencertan96 <spencertan_1220@hotmail.com>
2022-01-12 00:54:08 +0800

    remove print

--- utilities/offsetter.py
+++ utilities/offsetter.py
@@ -4,7 +4,6 @@ offset = 1
 output = ""
 with open(FILEPATH, 'r') as f:
     text = f.readline()
-    print(text)
     while text:
         text = text.split(":")
         output += f"{int(text[0]) + offset}:{text[1]}"
